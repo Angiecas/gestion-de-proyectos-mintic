bf2cb2e70039e0635cd79cb1de89b7c9d42c419f
Angiecas <avcastrogo@gmail.com>
2021-11-25 04:09:21 -0500

    agregar despliegue de dev

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,22 @@
 import type { NextPage } from 'next'
 import React from 'react';
 import Link from 'next/link';
+import Head from 'next/head';
+
 
 const Home: NextPage = () => {
   return (
     <div className= 'bg-green-500'>
+      <Head>
+        <title>Home | Angie</title>
+      </Head>
           <p>Página del Index principal</p>
       <Link href= '/admin/usuario'>
         <a className= 'cursor-pointer'> Ir para usuarios </a>
       </Link>
+      <div>
+        <i className = 'fas fa-atom' />
+        </div>
     </div>
   )
 }
